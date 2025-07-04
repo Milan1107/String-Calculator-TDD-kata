545abfcd2be244c00e983787372e0d4061a99636
Milan1107 <milan.gajera1107>
2025-07-04 20:03:47 +0530

    new line as delemeter with , numbers string executed

--- string_Calculator.py
+++ string_Calculator.py
@@ -1,9 +1,12 @@
 ###     This is stringCalculator.py     ###
 
+#   --- importing re for usage of reguler exp for ','|'\n' 
+import re
+
 class StringCalculator:
     def Add(self,numbers:str)->int:
         if numbers == "":
             return 0
         
-        parts = numbers.split(",")
+        parts = re.split(r",|\n",numbers)
         return sum(int(p) for p in parts)
--- test_String_Calculator.py
+++ test_String_Calculator.py
@@ -17,4 +17,10 @@ def test_two_numbers_return_sum():
 def test_unknown_amount_numbers_return_sum():
     sc = StringCalculator()
     assert sc.Add("1,2,3")==6
-    assert sc.Add("3,4,5,6,7")==27
+    assert sc.Add("3,4,5,6,7")==25
+
+### Defining test case through \n ###
+def test_newline_delimeter_sum():
+    sc = StringCalculator()
+    assert sc.Add("1\n2,3")==6
+    assert sc.Add("1\n4")==5
