1a3d4feae41041f580179a35b3211955a4a337ef
Milan1107 <milan.gajera1107>
2025-07-04 19:08:07 +0530

    Unknown amount of numbers string executed

--- test_String_Calculator.py
+++ test_String_Calculator.py
@@ -13,3 +13,8 @@ def test_single_number_return_itself():
 def test_two_numbers_return_sum():
     sc = StringCalculator()
     assert sc.Add("1,2")==3
+
+def test_unknown_amount_numbers_return_sum():
+    sc = StringCalculator()
+    assert sc.Add("1,2,3")==6
+    assert sc.Add("3,4,5,6,7")==27
